d6c52b614319f695672ea6a75fe39fd537a8bd3d
Adrien Smith <adrien@bouldersmiths.com>
2018-02-01 13:41:06 -0700

    Rename bot and fix run error

--- lib/c_bot.rb
+++ lib/c_bot.rb
@@ -7,6 +7,11 @@ require_relative 'config.rb'
 bot = Discordrb::Bot.new token: configatron.token,
                          client_id: '168123456789123456'
 
+# Events section
+
+# Adds event to kick any user who says uwu
 bot.message(with_text: /\buwu\b/i) do |event|
   event.server.kick(event.author)
 end
+
+bot.run
