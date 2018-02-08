9c0a2d349fb8b8cb33694f1ca7031738d2589d1f
Adrien Smith <adrien@bouldersmiths.com>
2018-02-08 16:48:18 -0700

    Begin refactoring

--- lib/c_bot.rb
+++ lib/c_bot.rb
@@ -4,74 +4,76 @@ require 'discordrb'
 require 'configatron'
 require_relative 'config.rb'
 
-bot = Discordrb::Bot.new token: configatron.token,
-                         client_id: '168123456789123456'
+module CornBot
+  BOT = Discordrb::Bot.new token: configatron.token,
+                           client_id: '168123456789123456'
 
-# Events section
+  # Events section
 
-# Adds event to kick any user who says uwu
-bot.message(with_text: /\buwu\b/i) do |event|
-  event.server.kick(event.author)
+  # Adds event to kick any user who says uwu
+  BOT.message(with_text: /\buwu\b/i) do |event|
+    event.server.kick(event.author)
+  end
+
+  # Adds event to play "just beat my dick" audio clip
+  BOT.message(content: '~beatmydick') do |event|
+    channel = event.user.voice_channel
+    next unless channel
+    BOT.voice_connect(channel)
+
+    voice_bot = event.voice
+    voice_bot.play_file('data/beatmydick.mp3')
+
+    voice_bot.destroy
+  end
+
+  # Adds event to play "arise chicken" audio clip
+  BOT.message(content: '~arisechicken') do |event|
+    channel = event.user.voice_channel
+    next unless channel
+    BOT.voice_connect(channel)
+
+    voice_bot = event.voice
+    voice_bot.play_file('data/arisechicken.mp3')
+
+    voice_bot.destroy
+  end
+
+  # Adds event to play "Penn's stfu" audio clip
+  BOT.message(content: '~stfu') do |event|
+    channel = event.user.voice_channel
+    next unless channel
+    BOT.voice_connect(channel)
+
+    voice_bot = event.voice
+    voice_bot.play_file('data/stfu.mp3')
+
+    voice_bot.destroy
+  end
+
+  # Adds event to play "Big Smoke moan" audio clip
+  BOT.message(content: '~smokemoan') do |event|
+    channel = event.user.voice_channel
+    next unless channel
+    BOT.voice_connect(channel)
+
+    voice_bot = event.voice
+    voice_bot.play_file('data/smokemoan.mp3')
+
+    voice_bot.destroy
+  end
+
+  # Adds event to play "bird up" audio clip
+  BOT.message(content: '~birdup') do |event|
+    channel = event.user.voice_channel
+    next unless channel
+    BOT.voice_connect(channel)
+
+    voice_bot = event.voice
+    voice_bot.play_file('data/birdup.mp3')
+
+    voice_bot.destroy
+  end
+
+  BOT.run
 end
-
-# Adds event to play "just beat my dick" audio clip
-bot.message(content: '~beatmydick') do |event|
-  channel = event.user.voice_channel
-  next unless channel
-  bot.voice_connect(channel)
-
-  voice_bot = event.voice
-  voice_bot.play_file('data/beatmydick.mp3')
-
-  voice_bot.destroy
-end
-
-# Adds event to play "arise chicken" audio clip
-bot.message(content: '~arisechicken') do |event|
-  channel = event.user.voice_channel
-  next unless channel
-  bot.voice_connect(channel)
-
-  voice_bot = event.voice
-  voice_bot.play_file('data/arisechicken.mp3')
-
-  voice_bot.destroy
-end
-
-# Adds event to play "Penn's stfu" audio clip
-bot.message(content: '~stfu') do |event|
-  channel = event.user.voice_channel
-  next unless channel
-  bot.voice_connect(channel)
-
-  voice_bot = event.voice
-  voice_bot.play_file('data/stfu.mp3')
-
-  voice_bot.destroy
-end
-
-# Adds event to play "Big Smoke moan" audio clip
-bot.message(content: '~smokemoan') do |event|
-  channel = event.user.voice_channel
-  next unless channel
-  bot.voice_connect(channel)
-
-  voice_bot = event.voice
-  voice_bot.play_file('data/smokemoan.mp3')
-
-  voice_bot.destroy
-end
-
-# Adds event to play "bird up" audio clip
-bot.message(content: '~birdup') do |event|
-  channel = event.user.voice_channel
-  next unless channel
-  bot.voice_connect(channel)
-
-  voice_bot = event.voice
-  voice_bot.play_file('data/birdup.mp3')
-
-  voice_bot.destroy
-end
-
-bot.run
