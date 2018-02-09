1918ac670dab8e413684a76dd2734100b2b08612
Adrien Smith <adrien@bouldersmiths.com>
2018-02-08 23:21:05 -0700

    Add iFunny watermark detection/removal

--- lib/c_bot.rb
+++ lib/c_bot.rb
@@ -2,10 +2,16 @@
 
 require 'discordrb'
 require 'configatron'
-require 'RMagick'
+require 'rmagick'
+require 'open-uri'
 require_relative 'config.rb'
 
 module CornBot
+  include Magick
+
+  WATERMARK = ImageList.new('data/images/ifunny_watermark.png')
+  WATERMARK.fuzz = '20%'
+
   BOT = Discordrb::Bot.new token: configatron.token,
                            client_id: '168123456789123456'
 
@@ -74,6 +80,23 @@ module CornBot
     voice_bot.play_file('data/birdup.mp3')
 
     voice_bot.destroy
+  end
+
+  # Add event to automatically detect and crop out iFunny watermarks
+  BOT.message do |event|
+    next if event.from_bot?
+    next unless event.message.attachments
+    original_images = ImageList.new
+    event.message.attachments.each do |attachment|
+      original_images << Image.from_blob(open(attachment.url).read).first if attachment.image?
+      original_images.each do |img|
+        img.fuzz = '20%'
+        next unless img.find_similar_region(WATERMARK, img.columns - 140, img.rows - 20)
+        cropped = img.crop(0, 0, img.columns, img.rows - 20, true)
+        cropped.write('data/images/cropped.jpg')
+        event << event.attach_file('data/images/cropped.jpg')
+      end
+    end
   end
 
   BOT.run
